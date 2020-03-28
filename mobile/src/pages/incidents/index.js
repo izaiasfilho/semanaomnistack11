79967e208b31cd0babb576b2b1982265e48fb672
izaiasfilho <izs.af@hotmail.com>
2020-03-27 23:42:20 -0300

    mobile teste

--- mobile/src/pages/incidents/index.js
+++ mobile/src/pages/incidents/index.js
@@ -31,10 +31,10 @@ export default function Incidents() {
         setIncidents([...incidents, ...response.data]);
         setTotal(response.headers['x-total-count']);
         setPage(page + 1);
-        //
+        
         setLoading(false);
     }
-
+//bla bla bla
     useEffect(() => {
         loadIncidents();
     }, []);
